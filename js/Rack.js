22e06d06b70038fed3487e2a510e7711768b16f2
Hai Nguyen <2nguyen.work@gmail.com>
2024-06-26 16:48:27 +0700

    feat: hide paint & config rack server

--- js/Rack.js
+++ js/Rack.js
@@ -4,7 +4,7 @@ export default class Rack {
   constructor(length, width, number) {
     this.length = length;
     this.width = width;
-    this.height = number * 10 + 2;
+    this.height = number * 15 + 10;
     this.number = number;
   }
 
@@ -13,7 +13,7 @@ export default class Rack {
 
     const blackMaterial = new THREE.MeshBasicMaterial({
       transparent: true,
-      opacity: 0.8,
+      opacity: 0.6,
       color: 0x000000,
     });
     // const whiteMaterial = new THREE.MeshBasicMaterial({
@@ -38,53 +38,133 @@ export default class Rack {
     const baseGeometry = new THREE.BoxGeometry(this.height, 5, this.width);
     const backGeometry = new THREE.BoxGeometry(this.length, 5, this.height);
 
-    // Tạo đáy và đỉnh màu đen
-    const bottom = new THREE.Mesh(shelfGeometry, blackMaterial);
-    bottom.position.y = 2;
-    rack.add(bottom);
+    const baseInstancedMesh = new THREE.InstancedMesh(
+      baseGeometry,
+      blackMaterial,
+      15
+    );
+    const matrix = new THREE.Matrix4();
+    const positionVector = new THREE.Vector3();
+    const rotationMatrix = new THREE.Matrix4();
 
-    const top = new THREE.Mesh(shelfGeometry, blackMaterial);
-    top.position.y = this.number * 10;
-    rack.add(top);
+    // Đặt ma trận cho instance 0
+    positionVector.set(-this.length / 2, this.height / 2, 0);
+    rotationMatrix.makeRotationZ(Math.PI / 2);
+    matrix.compose(
+      positionVector,
+      new THREE.Quaternion().setFromRotationMatrix(rotationMatrix),
+      new THREE.Vector3(1, 1, 1)
+    );
+    baseInstancedMesh.setMatrixAt(0, matrix);
 
-    // Tạo InstancedMesh cho các kệ ở giữa
-    const shelfCount = this.number - 1; // Số kệ ở giữa
-    if (shelfCount > 0) {
-      const shelfInstancedMesh = new THREE.InstancedMesh(
-        shelfGeometry,
-        materialsU,
-        shelfCount
+    // Đặt ma trận cho instance 1
+    positionVector.set(this.length / 2, this.height / 2, 0);
+    rotationMatrix.makeRotationZ(Math.PI / 2);
+    matrix.compose(
+      positionVector,
+      new THREE.Quaternion().setFromRotationMatrix(rotationMatrix),
+      new THREE.Vector3(1, 1, 1)
+    );
+    baseInstancedMesh.setMatrixAt(1, matrix);
+
+    // Đặt ma trận cho instance 2
+    positionVector.set(0, this.height / 2, -this.width / 2);
+    const euler = new THREE.Euler(Math.PI / 2, Math.PI / 2, 0, "XYZ");
+    const rotationMatrix2 = new THREE.Matrix4().makeRotationFromEuler(euler);
+    matrix.compose(
+      positionVector,
+      new THREE.Quaternion().setFromRotationMatrix(rotationMatrix2),
+      new THREE.Vector3(1, 1, this.length / this.width)
+    );
+    baseInstancedMesh.setMatrixAt(2, matrix);
+
+    // Đặt ma trận cho instance 3
+    positionVector.set(0, 2, 0);
+    matrix.compose(
+      positionVector,
+      new THREE.Quaternion().setFromRotationMatrix(new THREE.Matrix4()),
+      new THREE.Vector3(this.length / this.height, 1, 1)
+    );
+    baseInstancedMesh.setMatrixAt(3, matrix);
+
+    // Đặt ma trận cho instance 4
+    positionVector.set(0, this.height, 0);
+    matrix.compose(
+      positionVector,
+      new THREE.Quaternion().setFromRotationMatrix(new THREE.Matrix4()),
+      new THREE.Vector3(this.length / this.height, 1, 1)
+    );
+    baseInstancedMesh.setMatrixAt(4, matrix);
+
+    for (let i = 1; i <= this.number; i++) {
+      positionVector.set(0, i * 15 - 3, 0);
+      matrix.compose(
+        positionVector,
+        new THREE.Quaternion().setFromRotationMatrix(new THREE.Matrix4()),
+        new THREE.Vector3(this.length / this.height, 0.3, 1)
       );
-
-      // Ma trận để thiết lập vị trí cho mỗi instance
-      const matrix = new THREE.Matrix4();
-
-      // Thiết lập vị trí cho mỗi kệ ở giữa
-      for (let i = 0; i < shelfCount; i++) {
-        matrix.setPosition(0, (i + 1) * 10, 0);
-        shelfInstancedMesh.setMatrixAt(i, matrix);
-      }
-
-      rack.add(shelfInstancedMesh);
+      baseInstancedMesh.setMatrixAt(i + 4, matrix);
     }
 
-    // Tạo các phần đứng
-    const base1 = new THREE.Mesh(baseGeometry, blackMaterial);
-    const base2 = new THREE.Mesh(baseGeometry, blackMaterial);
+    // Cập nhật ma trận instance
+    baseInstancedMesh.instanceMatrix.needsUpdate = true;
+    baseInstancedMesh.name = "rack";
+    rack.add(baseInstancedMesh);
 
-    base1.rotateZ(THREE.Math.degToRad(90));
-    base2.rotateZ(THREE.Math.degToRad(90));
+    for (let i = 0; i < this.number; i++) {
+      const serverU = new THREE.Mesh(shelfGeometry, materialsU);
+      serverU.position.y = (i + 1) * 15;
+      serverU.name = "serverU " + (i + 1);
+      rack.add(serverU);
+    }
 
-    base1.position.set(-this.length / 2, this.height / 2, 0);
-    base2.position.set(this.length / 2, this.height / 2, 0);
+    // Tạo đáy và đỉnh màu đen
+    // const bottom = new THREE.Mesh(shelfGeometry, blackMaterial);
+    // bottom.position.y = 2;
+    // rack.add(bottom);
 
-    rack.add(base1, base2);
+    // const top = new THREE.Mesh(shelfGeometry, blackMaterial);
+    // top.position.y = this.number * 10;
+    // rack.add(top);
 
-    // Tạo phần sau
-    const back = new THREE.Mesh(backGeometry, blackMaterial);
-    back.rotateX(THREE.Math.degToRad(90));
-    back.position.set(0, this.height / 2, -this.width / 2);
-    rack.add(back);
+    // // Tạo InstancedMesh cho các kệ ở giữa
+    // const shelfCount = this.number - 1; // Số kệ ở giữa
+    // if (shelfCount > 0) {
+    //   const shelfInstancedMesh = new THREE.InstancedMesh(
+    //     shelfGeometry,
+    //     materialsU,
+    //     shelfCount
+    //   );
+
+    //   // Ma trận để thiết lập vị trí cho mỗi instance
+    //   const matrix = new THREE.Matrix4();
+
+    //   // Thiết lập vị trí cho mỗi kệ ở giữa
+    //   for (let i = 0; i < shelfCount; i++) {
+    //     matrix.setPosition(0, (i + 1) * 10, 0);
+    //     shelfInstancedMesh.setMatrixAt(i, matrix);
+    //   }
+
+    //   rack.add(shelfInstancedMesh);
+    // }
+
+    // // Tạo các phần đứng
+    // const base1 = new THREE.Mesh(baseGeometry, blackMaterial);
+    // const base2 = new THREE.Mesh(baseGeometry, blackMaterial);
+
+    // base1.rotateZ(THREE.Math.degToRad(90));
+    // base2.rotateZ(THREE.Math.degToRad(90));
+
+    // base1.position.set(-this.length / 2, this.height / 2, 0);
+    // base2.position.set(this.length / 2, this.height / 2, 0);
+
+    // rack.add(base1, base2);
+
+    // // Tạo phần sau
+    // const back = new THREE.Mesh(backGeometry, blackMaterial);
+    // back.rotateX(THREE.Math.degToRad(90));
+    // back.position.set(0, this.height / 2, -this.width / 2);
+    // rack.add(back);
 
     return rack;
   }
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { DragControls } from "three/examples/jsm/controls/DragControls.js";
 import { TransformControls } from "three/examples/jsm/controls/TransformControls.js";
-import Paint from "./paint.js";
+// import Paint from "./paint.js";
 import Rack from "./Rack.js";
 
 const scene = new THREE.Scene();
@@ -11,7 +11,7 @@ const camera = new THREE.PerspectiveCamera(
   20,
   window.innerWidth / window.innerHeight,
   0.1,
-  100000
+  10000
 );
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
@@ -21,9 +21,11 @@ const endButton = document.getElementById("endButton");
 const clearButton = document.getElementById("clearButton");
 const moveButton = document.getElementById("moveButton");
 const addButton = document.getElementById("addButton");
+const tooltip = document.getElementById("tooltip");
+
 clearButton.addEventListener("click", clearPlanes);
-endButton.addEventListener("click", drawPlane);
-moveButton.addEventListener("click", moveObjects);
+// endButton.addEventListener("click", drawPlane);
+// moveButton.addEventListener("click", moveObjects);
 addButton.addEventListener("click", addRacks);
 
 let movable = false;
@@ -40,154 +42,236 @@ function clearPlanes() {
     }
   });
 
-  while (scene.children.length) {
-    const child = scene.children[0];
-    // Check if the child has material(s) and dispose of it/them
-    if (child.material) {
-      if (Array.isArray(child.material)) {
-        child.material.forEach((material) => {
-          material.dispose();
-        });
-      } else {
-        child.material.dispose();
-      }
-    }
+  // while (scene.children.length) {
+  //   const child = scene.children[0];
+  //   // Check if the child has material(s) and dispose of it/them
+  //   if (child.material) {
+  //     if (Array.isArray(child.material)) {
+  //       child.material.forEach((material) => {
+  //         material.dispose();
+  //       });
+  //     } else {
+  //       child.material.dispose();
+  //     }
+  //   }
 
-    // If the child has a texture, dispose of it
-    if (child.texture) child.texture.dispose();
-    scene.remove(child);
-  }
+  //   // If the child has a texture, dispose of it
+  //   if (child.texture) child.texture.dispose();
+  //   scene.remove(child);
+  // }
 
-  for (let dragControl of dragControls) {
-    dragControl.dispose();
-    dragControl = null;
-  }
+  // dragControls.dispose();
 
   for (let rack of racks) {
-    transformControls.detach(rack);
+    // transformControls.detach(rack);
     scene.remove(rack);
-    rack = null;
+    // rack = null;
   }
+  console.log(scene);
   tmpPositionX = -100;
-  tmpPositionZ = -10;
+  // tmpPositionZ = -10;
   scene.add(lightHelper, gridHelper);
 
   drawedPlanes.length = 0;
   points = [];
   racks = [];
-  dragControls = [];
+  draggableServerU = [];
+
+  dragControls.dispose();
+  dragControls = new DragControls(
+    draggableServerU,
+    camera,
+    renderer.domElement
+  );
+  snapPostions = [];
+  dragControls.addEventListener("dragstart", disableOrbitControls);
+  dragControls.addEventListener("dragend", enableOrbitControls);
+  dragControls.addEventListener("drag", snapToRack);
+  dragControls.addEventListener("hoveron", displayBox);
+  dragControls.addEventListener("hoveroff", hideBox);
 }
 
-function drawPlane() {
-  points = Paint();
-  const extrudeSettings = {
-    steps: 1,
-    depth: 70,
-    bevelEnabled: false,
-  };
-  for (let j = 0; j < points.length; j++) {
-    for (let i = 0; i < points[j].length - 1; i++) {
-      const shape = new THREE.Shape();
+// function drawPlane() {
+//   points = Paint();
+//   const extrudeSettings = {
+//     steps: 1,
+//     depth: 70,
+//     bevelEnabled: false,
+//   };
+//   for (let j = 0; j < points.length; j++) {
+//     for (let i = 0; i < points[j].length - 1; i++) {
+//       const shape = new THREE.Shape();
 
-      shape.moveTo((points[j][i].x - 500) / 2, (points[j][i].y - 250) / 2);
-      shape.lineTo(
-        (points[j][i + 1].x - 500) / 2,
-        (points[j][i + 1].y - 250) / 2
-      );
+//       shape.moveTo((points[j][i].x - 500) / 2, (points[j][i].y - 250) / 2);
+//       shape.lineTo(
+//         (points[j][i + 1].x - 500) / 2,
+//         (points[j][i + 1].y - 250) / 2
+//       );
 
-      const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-      // const material = new THREE.MeshBasicMaterial({
-      //   color: 0x00ff00,
-      // });
-      // const mesh = new THREE.Mesh(geometry, material);
-      const edges3 = new THREE.EdgesGeometry(geometry);
-      const line3 = new THREE.LineSegments(
-        edges3,
-        new THREE.LineBasicMaterial({ color: 0xffffff })
-      );
-      line3.rotateX(THREE.Math.degToRad(90));
-      line3.position.y = 70;
-      // objects.push(line3);
-      drawedPlanes.push(line3);
-      scene.add(line3);
-    }
-  }
-}
+//       const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+//       // const material = new THREE.MeshBasicMaterial({
+//       //   color: 0x00ff00,
+//       // });
+//       // const mesh = new THREE.Mesh(geometry, material);
+//       const edges3 = new THREE.EdgesGeometry(geometry);
+//       const line3 = new THREE.LineSegments(
+//         edges3,
+//         new THREE.LineBasicMaterial({ color: 0xffffff })
+//       );
+//       line3.rotateX(THREE.Math.degToRad(90));
+//       line3.position.y = 70;
+//       // objects.push(line3);
+//       drawedPlanes.push(line3);
+//       scene.add(line3);
+//     }
+//   }
+// }
 
-function moveObjects() {
-  movable = !movable;
-  transformControls.showX = movable;
-  transformControls.showY = movable;
-  transformControls.showZ = movable;
-}
+// function moveObjects() {
+//   movable = !movable;
+//   transformControls.showX = movable;
+//   transformControls.showY = movable;
+//   transformControls.showZ = movable;
+// }
 
 let tmpPositionX = -100;
-let tmpPositionZ = -10;
+// let tmpPositionZ = -10;
+let draggableServerU = [];
 let racks = [];
-let dragControls = [];
+let snapPostions = [];
 function addRacks() {
-  let rackTmp;
-
-  let rackMesh = new Rack(
+  const rackMesh = new Rack(
     document.getElementById("length").value,
     document.getElementById("width").value,
     document.getElementById("number").value
   );
-  rackTmp = rackMesh.createRack();
+  const rackTmp = rackMesh.createRack();
   rackTmp.position.x = tmpPositionX;
-  rackTmp.position.z = tmpPositionZ;
-  tmpPositionX -= document.getElementById("length").value;
-  tmpPositionZ -= document.getElementById("width").value;
-  const boxHelper = new THREE.BoxHelper(rackTmp, 0x00ffff); // Màu khung là vàng
-  scene.add(boxHelper);
+  // rackTmp.position.z = tmpPositionZ;
+  tmpPositionX -= document.getElementById("length").value * 2;
+  // tmpPositionZ -= document.getElementById("width").value;
   scene.add(rackTmp);
   racks.push(rackTmp);
+  draggableServerU.push(...Array.from(rackTmp.children).slice(1));
 
-  let dragControls2 = new DragControls([rackTmp], camera, renderer.domElement);
-  dragControls2.transformGroup = true;
-
-  dragControls2.addEventListener("hoveron", function () {
-    transformControls.attach(rackTmp);
-  });
-
-  dragControls2.addEventListener("dragstart", disableOrbitControls);
-  dragControls2.addEventListener("dragend", enableOrbitControls);
-  let previousPosition;
-
-  dragControls2.addEventListener("dragstart", function () {
-    disableOrbitControls();
-    previousPosition = rackTmp.position.clone(); // Lưu lại vị trí ban đầu của rack
-  });
-
-  dragControls2.addEventListener("dragend", function () {
-    enableOrbitControls();
-    if (checkRackCollisions(rackTmp)) {
-      rack.position.copy(previousPosition); // Hoàn tác vị trí của rack về vị trí cuối cùng trước khi có va chạm
-    }
-  });
-
-  dragControls.push(dragControls2);
-
-  animate();
-}
-
-function checkRackCollisions(draggedRack) {
-  const draggedRackBox = new THREE.Box3().setFromObject(draggedRack);
-
-  for (const rack of racks) {
-    if (rack !== draggedRack) {
-      const rackBox = new THREE.Box3().setFromObject(rack);
-
-      if (draggedRackBox.intersectsBox(rackBox)) {
-        // Xử lý va chạm ở đây
-        // Ví dụ: Ngăn chặn việc di chuyển rack đang kéo
-        return true;
-      }
+  for (let i = 0; i < rackTmp.children.length; i++) {
+    const p = rackTmp.children[i];
+    if (p.name.includes("serverU")) {
+      snapPostions.push(
+        new THREE.Vector3(p.position.x, p.position.y, p.position.z)
+      );
     }
   }
 
-  return false;
+  //console.log(rackMesh.getPositionList());
+  // const dragControls = new DragControls(
+  //   Array.from(rackTmp.children).slice(1),
+  //   camera,
+  //   renderer.domElement
+  // );
+  // dragControls2.transformGroup = true;
+
+  // dragControls2.addEventListener("hoveron", function (event) {
+  //   transformControls.attach(rackTmp);
+  //   if (event.object.name == "serverU") {
+  //     dragControls2.enabled = true;
+  //     dragControls2.transformGroup = false;
+  //   } else {
+  //     dragControls2.enabled = false;
+  //   }
+  // });
+
+  // let previousPosition;
+
+  // dragControls2.addEventListener("dragstart", function () {
+  //   disableOrbitControls();
+  //   previousPosition = rackTmp.position.clone(); // Lưu lại vị trí ban đầu của rack
+  // });
+
+  // dragControls2.addEventListener("dragend", function () {
+  //   enableOrbitControls();
+  //   if (checkRackCollisions(rackTmp)) {
+  //     rackTmp.position.copy(previousPosition); // Hoàn tác vị trí của rack về vị trí cuối cùng trước khi có va chạm
+  //   }
+  // });
 }
+
+// function checkRackCollisions(draggedRack) {
+//   const draggedRackBox = new THREE.Box3().setFromObject(draggedRack);
+
+//   for (const rack of racks) {
+//     if (rack !== draggedRack) {
+//       const rackBox = new THREE.Box3().setFromObject(rack);
+
+//       if (draggedRackBox.intersectsBox(rackBox)) {
+//         // Xử lý va chạm ở đây
+//         // Ví dụ: Ngăn chặn việc di chuyển rack đang kéo
+//         return true;
+//       }
+//     }
+//   }
+
+//   return false;
+// }
+let dragControls = new DragControls(
+  draggableServerU,
+  camera,
+  renderer.domElement
+);
+
+const enableOrbitControls = function () {
+  orbitControls.enabled = true;
+};
+
+const disableOrbitControls = function () {
+  orbitControls.enabled = false;
+};
+
+const snapToRack = function (event) {
+  snapPostions.forEach((target) => {
+    if (event.object.position.distanceTo(target) < 13) {
+      event.object.position.copy(target);
+      // if (event.object.position.distanceTo(target) < 1) {
+      //   let box = new THREE.BoxHelper(event.object, 0xffff00);
+      //   scene.add(box);
+      // }
+    }
+  });
+};
+
+const displayBox = function (event) {
+  const box = new THREE.BoxHelper(event.object, 0xffff00);
+  scene.add(box);
+
+  const info = getObjectInfo(event.object);
+  tooltip.innerHTML = info;
+  // Hiển thị tooltip
+  tooltip.style.display = "block";
+};
+
+const hideBox = function (event) {
+  scene.remove(scene.children[scene.children.length - 1]);
+  tooltip.style.display = "none";
+};
+
+function getObjectInfo(object) {
+  // Hàm này trả về thông tin bạn muốn hiển thị trong tooltip
+  // Ví dụ:
+  return `
+  <strong>Id:</strong> ${object.id}<br><br>
+  <strong>Name:</strong> ${object.name}<br><br>
+  <strong>Position:</strong> ${object.position.toArray().join(", ")}<br><br>
+  <strong>UUID:</strong> ${object.uuid}<br><br>
+  <strong>Type:</strong> ${object.type}<br><br>
+  <strong>Temperature:</strong> 80ºC<br><br>
+`;
+}
+
+dragControls.addEventListener("dragstart", disableOrbitControls);
+dragControls.addEventListener("dragend", enableOrbitControls);
+dragControls.addEventListener("drag", snapToRack);
+dragControls.addEventListener("hoveron", displayBox);
+dragControls.addEventListener("hoveroff", hideBox);
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -207,52 +291,48 @@ scene.add(lightHelper, gridHelper);
 const spaceTexture = new THREE.TextureLoader().load("space.jpg");
 scene.background = spaceTexture;
 
-const transformControls = new TransformControls(camera, renderer.domElement);
+// const transformControls = new TransformControls(camera, renderer.domElement);
 
-scene.add(transformControls);
+// scene.add(transformControls);
 
-transformControls.addEventListener("dragging-changed", function (event) {
-  orbitControls.enabled = !event.value;
-});
+// transformControls.addEventListener("dragging-changed", function (event) {
+//   orbitControls.enabled = !event.value;
+// });
 
-transformControls.showX = false;
-transformControls.showY = false;
-transformControls.showZ = false;
-
-const enableOrbitControls = function () {
-  orbitControls.enabled = true;
-};
-
-const disableOrbitControls = function () {
-  orbitControls.enabled = false;
-};
+// transformControls.showX = false;
+// transformControls.showY = false;
+// transformControls.showZ = false;
 
 function animate() {
   requestAnimationFrame(animate);
   for (const rack of racks) {
-    rack.position.clamp(
-      new THREE.Vector3(-1000, 0, -1000),
-      new THREE.Vector3(1000, 1000, 1000)
-    );
+    rack.children.forEach((child) => {
+      if (child.name.includes("serverU")) {
+        child.position.clamp(
+          new THREE.Vector3(-1000, 0, 0),
+          new THREE.Vector3(1000, 1000, 0)
+        );
+      }
+    });
   }
   renderer.render(scene, camera);
 }
 
 //Racks
 
-let positionX = -100;
-let positionZ = -10;
-for (let i = 1; i <= 20; i++) {
-  let rackMesh = new Rack(70, 50, 10);
-  let rackTmp = rackMesh.createRack();
-  rackTmp.position.x = positionX;
-  rackTmp.position.z = positionZ;
-  positionZ -= 100;
-  scene.add(rackTmp);
-  if (i % 5 === 0) {
-    positionX -= 150;
-    positionZ = -10;
-  }
-}
+// let positionX = 200;
+// let positionZ = 200;
+// for (let i = 1; i <= 20; i++) {
+//   let rackMesh = new Rack(70, 50, 10);
+//   let rackTmp = rackMesh.createRack();
+//   rackTmp.position.x = positionX;
+//   rackTmp.position.z = positionZ;
+//   positionZ -= 100;
+//   scene.add(rackTmp);
+//   if (i % 5 === 0) {
+//     positionX -= 150;
+//     positionZ = 200;
+//   }
+// }
 
 animate();
